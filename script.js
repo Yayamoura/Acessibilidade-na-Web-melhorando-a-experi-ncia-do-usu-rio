0318160dd56034c0dc67ec274a2721c8075eef84
Yayamoura <00001114213329sp@al.educacao.sp.gov.br>
2025-08-12 19:46:57 -0300

    aula 5 completa

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
+     botaoDeAcessibilidade.addEventListener('click', function(){
+       botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+       opcoesDaAcessibilidade.classl
+     })
+  
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
