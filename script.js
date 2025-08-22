352c65afaf0dea9e386268c00d1d87d907247637
Yayamoura <00001114213329sp@al.educacao.sp.gov.br>
2025-08-22 19:34:59 -0300

    aula 6 completa

--- script.js
+++ script.js
@@ -33,3 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 })
+
+ alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ })
